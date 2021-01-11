349c2b1c0654114e8fbb79e8451f4214ad9664cb
ThiagoHenriqueCastro <thiago.castro.oliveira@outlook.com>
2021-01-11 11:04:38 -0300

    ContinueWatchingCard

--- lib/screens/continue_watching_screen.dart
+++ lib/screens/continue_watching_screen.dart
@@ -1,4 +1,5 @@
 import 'package:designpluscode/constants.dart';
+import 'package:designpluscode/model/course.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
@@ -46,11 +47,175 @@ class ContinueWatchingScreen extends StatelessWidget {
               style: kTitle2Style,
             ),
           ),
+          ContinueWatchingList(),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 32),
             child: Text(
               "Certificates",
               style: kTitle2Style,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ContinueWatchingList extends StatefulWidget {
+  @override
+  _ContinueWatchingListState createState() => _ContinueWatchingListState();
+}
+
+class _ContinueWatchingListState extends State<ContinueWatchingList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: continueWatchingCourses.map((course) {
+        var index = continueWatchingCourses.indexOf(course);
+        return Container(
+          width: 7,
+          height: 7,
+          margin: EdgeInsets.symmetric(horizontal: 6),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 260,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return ContinueWatchingCard(
+                course: continueWatchingCourses[index],
+              );
+            },
+            itemCount: continueWatchingCourses.length,
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+            controller: PageController(initialPage: 0, viewportFraction: 0.75),
+          ),
+        ),
+        updateIndicators()
+      ],
+    );
+  }
+}
+
+class ContinueWatchingCard extends StatelessWidget {
+  ContinueWatchingCard({this.course});
+
+  final Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20, right: 20),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: course.background,
+                borderRadius: BorderRadius.circular(41),
+                boxShadow: [
+                  BoxShadow(
+                      color: course.background.colors[0].withOpacity(0.3),
+                      offset: Offset(0, 20),
+                      blurRadius: 30),
+                  BoxShadow(
+                      color: course.background.colors[1].withOpacity(0.3),
+                      offset: Offset(0, 20),
+                      blurRadius: 30),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(41),
+                child: Container(
+                  height: 140,
+                  width: 260,
+                  child: Stack(
+                    children: [
+                      Row(
+                        children: [
+                          Spacer(),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Image.asset(
+                                'asset/illustrations/${course.illustration}',
+                                fit: BoxFit.cover,
+                                height: 110,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(
+                          32,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(course.courseSubtitle,
+                                style: kCardSubtitleStyle),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              course.courseTitle,
+                              style: kCardTitleStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+                boxShadow: [
+                  BoxShadow(
+                    color: kShadowColor,
+                    offset: Offset(0, 4),
+                    blurRadius: 16,
+                  ),
+                ]),
+            padding: EdgeInsets.only(
+              top: 12,
+              bottom: 13.5,
+              left: 20.5,
+              right: 14.5,
+            ),
+            child: Image.asset(
+              'asset/icons/icon-play.png',
+              height: 60,
+              width: 60,
             ),
           )
         ],
